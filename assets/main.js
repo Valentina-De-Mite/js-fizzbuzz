ed89c4ca3132de5c4b8c295a038b4a7dc93867ab
Valentina De Mite <valesakura97@gmail.com>
2023-08-25 15:47:31 +0200

    added fizz-buzz in js

--- assets/main.js
+++ assets/main.js
@@ -14,3 +14,17 @@ Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un ele
 BONUS 2:
 Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
 */
+
+for (let i = 1; i < 101; i++) {
+  if (i % 15 === 0) {
+    console.log("Fizz-Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+// Strumenti
